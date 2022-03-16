6e7deec27229ae354dbe2790f4c417f67ac5daa0
razlevi <razlevio.55@gmail.com>
2022-03-16 19:27:12 +0200

    structure

---
+++ script.js
@@ -0,0 +1,36 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+// Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+
+function operate(num1, operator, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+      break;
+    case "-":
+      return subtract(num1, num2);
+      break;
+    case "*":
+      return multiply(num1, num2);
+      break;
+    case "/":
+      return divide(num1, num2);
+      break;
+    default:
+      console.log("Wrong input for OPERATE function");
+  }
+}
